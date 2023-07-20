5ef96c27b933c35e7527b4fded5df3106afcfedc
Basdlwaschdl <ftpobi@googlemail.com>
2023-07-21 00:06:22 +0200

    logo animation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,10 +8,17 @@ import { FirestoreService } from '../firestore.service';
 })
 export class LoginComponent implements OnInit {
 
+  animationStart: boolean = false
+  animationLogo: boolean = false
+  d_none:boolean = false
+
   constructor( private firestoreService: FirestoreService) {}
 
   ngOnInit(): void {
     this.firestoreService.updateData();
+    setTimeout(() =>  this.animationStart = true, 1000);
+    setTimeout(() =>  this.animationLogo = true, 2000);
+    setTimeout(() =>  this.d_none = true, 2900);
   }
  
 }
