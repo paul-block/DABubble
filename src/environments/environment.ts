1c68e5efd47387805ad0c07e4beba385fbf324ce
PhilippRandau <philipp-randau@web.de>
2023-07-20 19:25:25 +0200

    created firebase project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dabubble-f0c5b',
+    appId: '1:681646405483:web:ca06fd189a73328e9f7e2e',
+    storageBucket: 'dabubble-f0c5b.appspot.com',
+    apiKey: 'AIzaSyDose1H_0Pbm__bAvyddd5XgWqoTX1MocE',
+    authDomain: 'dabubble-f0c5b.firebaseapp.com',
+    messagingSenderId: '681646405483',
+  },
+};
