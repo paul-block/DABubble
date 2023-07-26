1563bdcc8bd80efe0b75fee9c83381d982cc7212
Cuise95 <paul_block@gmx.de>
2023-07-26 12:25:21 +0200

    added toggle functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/channel-sidebar/channel-sidebar.component.ts
+++ src/app/channel-sidebar/channel-sidebar.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-channel-sidebar',
   templateUrl: './channel-sidebar.component.html',
-  styleUrls: ['./channel-sidebar.component.scss']
+  styleUrls: ['./channel-sidebar.component.scss'],
 })
 export class ChannelSidebarComponent {
+  channelsVisible: boolean = true;
+  dmsVisible: boolean = true;
+  workspaceVisible: boolean = true;
 
+  toggleChannels() {
+    this.channelsVisible = !this.channelsVisible;
+  }
+
+  toggleDms() {
+    this.dmsVisible = !this.dmsVisible;
+  }
+
+  toggleWorkspace() {
+    this.workspaceVisible = !this.workspaceVisible;
+  }
 }
