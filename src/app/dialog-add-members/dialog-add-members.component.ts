14525915d3e5528f66fd04e61e0c3a830592e7c8
PhilippRandau <philipp-randau@web.de>
2023-07-28 18:17:08 +0200

    search add people channel

--- src/app/dialog-add-members/dialog-add-members.component.ts
+++ src/app/dialog-add-members/dialog-add-members.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-add-members.component.scss']
 })
 export class DialogAddMembersComponent {
-  
+  inputSearch: boolean = false;
+  inputSearchPeople: string = '';
+
+  checkSearchPeopleInput() {
+    if (this.inputSearchPeople === '') {
+      this.inputSearch = false;
+    } else {
+      this.inputSearch = true;
+    }
+  }
 }
