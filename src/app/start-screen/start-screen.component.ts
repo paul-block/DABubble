b915b1cb0bb56c93baa879ac0059432e27965f2d
Basdlwaschdl <ftpobi@googlemail.com>
2023-07-30 22:25:25 +0200

    responsive

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -20,9 +20,9 @@ export class StartScreenComponent implements OnInit {
 
   ngOnInit(): void {    
     setTimeout(() => this.animationStart = true, 1000);
-   // setTimeout(() => this.animationLogo = true, 2000);
-   // setTimeout(() => this.d_none = true, 2900);
-   // this.router.navigateByUrl('/sign-in')
+    setTimeout(() => this.animationLogo = true, 2000);
+    setTimeout(() => this.d_none = true, 2900);
+    this.router.navigateByUrl('/sign-in')
   }
 
 }
