a3c1237fb44e177eb6a5692d8f03d09b2f1ff355
Basdlwaschdl <ftpobi@googlemail.com>
2023-08-03 14:40:21 +0200

    thread header

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-thread',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ThreadComponent {
 
+  @Output() threadClose = new EventEmitter<boolean>();
+
+  closeThread(value: boolean) {
+    this.threadClose.emit(value)
+  }
 }
