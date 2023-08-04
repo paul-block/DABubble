3f539064cdff66871fb0ae698f3fe1a5bbc86f6e
Basdlwaschdl <ftpobi@googlemail.com>
2023-08-04 16:02:48 +0200

    emoji funktion

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { emojis } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+import { AuthenticationService } from '../authentication.service';
 
 
 @Component({
@@ -7,25 +8,65 @@ import { emojis } from '@ctrl/ngx-emoji-mart/ngx-emoji';
   templateUrl: './thread.component.html',
   styleUrls: ['./thread.component.scss']
 })
-export class ThreadComponent {
+export class ThreadComponent implements OnInit {
+  emoji_exist: boolean;
 
-  
+
+  constructor(public authenticationService: AuthenticationService) { }
+
   @Output() threadClose = new EventEmitter<boolean>();
-  emoji: any;
+  selectedEmoji: string
+  emojiPicker_open: boolean = false;
+  emojis = []
 
+  ngOnInit(): void {
+    document.body.addEventListener('click', this.bodyClicked);
+  }
 
   closeThread(value: boolean) {
     this.threadClose.emit(value)
   }
 
 
-  addEmoji($event) {
-    console.log($event.emoji);
-    
-    this.emoji = $event.emoji.native
- 
-    
-   
-
+  openEmojiPicker() {
+    this.emojiPicker_open = true
   }
+
+
+  addEmoji($event: any) {
+    this.emoji_exist = false
+    this.emojiPicker_open = false
+    this.checkIfEmojyExist($event.emoji.colons)
+    if (!this.emoji_exist) {
+      let emoji = {
+        emoji: $event.emoji.colons,
+        count: 1,
+        uid: this.authenticationService.userData.user_name
+      }
+      this.emojis.push(emoji)
+      console.log(this.emojis);
+    }
+  };
+
+
+  checkIfEmojyExist(emoji: string) {
+      this.emojis.forEach(element => {
+      if (element.emoji == emoji) {
+        element.count += 1
+        this.emoji_exist = true
+        console.log(this.emojis);
+      }
+    });
+  }
+
+
+  bodyClicked = () => {
+     if (this.emojiPicker_open == true) this.emojiPicker_open = false;
+  };
+
+  stopPropagation(event: Event) {
+       event.stopPropagation();
+  };
+
 }
+
