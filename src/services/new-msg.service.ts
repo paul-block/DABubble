b3ee47c83d694a72d61c08974c27754049662883
Cuise95 <paul_block@gmx.de>
2023-08-17 11:20:12 +0200

    extended for msg btn usage

--- src/services/new-msg.service.ts
+++ src/services/new-msg.service.ts
@@ -8,10 +8,17 @@ export class NewMsgService {
   private _openNewMsg = new BehaviorSubject<boolean>(false);
   public readonly openNewMsg$ = this._openNewMsg.asObservable();
 
+  user_name: string;
+  directedFromProfileButton:boolean = false;
+
 
   constructor() { }
 
   toggleNewMsg() {
     this._openNewMsg.next(!this._openNewMsg.getValue());
+  }
+
+  openNewMsgComponent() {
+   // funktion die immer öffnet, nicht toggled 
   }
 }
