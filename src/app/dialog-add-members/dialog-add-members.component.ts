5b08928e4da99fd9e7b7d56bf59cd984cac6df20
Cuise95 <paul_block@gmx.de>
2023-09-19 15:27:13 +0200

    add more members at once to existing channel

--- src/app/dialog-add-members/dialog-add-members.component.ts
+++ src/app/dialog-add-members/dialog-add-members.component.ts
@@ -1,3 +1,97 @@
+// import { Component, OnInit } from '@angular/core';
+// import { AuthenticationService } from 'services/authentication.service';
+// import { ChannelService } from 'services/channel.service';
+// import { MatDialogRef } from '@angular/material/dialog';
+// import { ChatService } from 'services/chat.service';
+// import { MessagesService } from 'services/messages.service';
+// import { UploadService } from 'services/upload.service';
+
+
+// @Component({
+//   selector: 'app-dialog-add-members',
+//   templateUrl: './dialog-add-members.component.html',
+//   styleUrls: ['./dialog-add-members.component.scss']
+// })
+// export class DialogAddMembersComponent implements OnInit {
+//   inputSearchUser: string = '';
+//   choosedUser: boolean = false;
+//   selectedUsers = [];
+//   users: any[] = [];
+//   filteredUsers: any
+//   currentChannel: string = '';
+
+//   constructor(
+//     private authService: AuthenticationService,
+//     public channelService: ChannelService,
+//     public dialogRef: MatDialogRef<DialogAddMembersComponent>,
+//     public chatService: ChatService,
+//     public messageService: MessagesService,
+//     public uploadService: UploadService
+//   ) { }
+
+//   async ngOnInit(): Promise<void> {
+//     this.users = await this.filterUserAllreadyAssigned()
+//   }
+
+
+//   filterUsers() {
+//     if (this.inputSearchUser.length > 0) {
+//       this.filteredUsers = this.users.filter(user =>
+//         user.user_name.toLowerCase().startsWith(this.inputSearchUser.toLowerCase())
+//       );
+//     } else {
+//       this.filteredUsers = this.users;
+//       if (this.filteredUsers.length === 0) console.log('kein username gefunden');
+
+//     }
+//   }
+
+
+//   selectUser(user) {
+//     this.choosedUser = true;
+//     this.inputSearchUser = user.user_name;
+//     this.selectedUsers.push(user);
+//   }
+
+
+//   addNewMember() {
+//     this.selectedUsers.forEach(user => {
+//       this.channelService.addUserToChannel(this.chatService.currentChatData.channelName, user.uid);
+//       this.sendAddMemberMessage(user.user_name);
+//     });
+//     this.selectedUsers = [];
+//     this.choosedUser = null;
+//     this.inputSearchUser = '';
+//     this.dialogRef.close();
+//   }
+
+
+//   clearInputName(userToRemove) {
+//     this.inputSearchUser = ''
+//     this.choosedUser = false;
+//     this.selectedUsers = this.selectedUsers.filter(user => user.uid !== userToRemove.uid);
+//   }
+
+
+//   async filterUserAllreadyAssigned(): Promise<any> {
+//     let users = await this.authService.getAllUsers();
+//     this.channelService.currentChannelData.assignedUsers.forEach((assignedUser: any) => {
+//       let user = users.find(element => element.uid === assignedUser)
+//       let index = users.indexOf(user)
+//       users.splice(index, 1)
+//     });
+//     return users
+//   }
+
+
+//   sendAddMemberMessage(user: string) {
+//     this.uploadService.checkForUpload()
+//     this.messageService.messageText = user + ' ist #' + this.channelService.currentChannelData.channelName + ' beigetreten.'
+//     this.messageService.newMessage()
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'services/authentication.service';
 import { ChannelService } from 'services/channel.service';
@@ -15,10 +109,12 @@ import { UploadService } from 'services/upload.service';
 export class DialogAddMembersComponent implements OnInit {
   inputSearchUser: string = '';
   choosedUser: boolean = false;
+  userAlreadyAdded: boolean = false;
   selectedUsers = [];
   users: any[] = [];
   filteredUsers: any
   currentChannel: string = '';
+  loading: boolean = true;
 
   constructor(
     private authService: AuthenticationService,
@@ -29,8 +125,11 @@ export class DialogAddMembersComponent implements OnInit {
     public uploadService: UploadService
   ) { }
 
+
   async ngOnInit(): Promise<void> {
-    this.users = await this.filterUserAllreadyAssigned()
+    this.users = await this.filterAlreadyAssignedUsers()
+    this.filterUsers()
+    this.loading = false;
   }
 
 
@@ -48,32 +147,33 @@ export class DialogAddMembersComponent implements OnInit {
 
 
   selectUser(user) {
-    this.choosedUser = true;
-    this.inputSearchUser = user.user_name;
-    this.selectedUsers.push(user);
+    if (this.inputSearchUser.length > 0 && !this.selectedUsers.includes(user)) {
+      this.userAlreadyAdded = false;
+      this.selectedUsers.push(user);
+      this.inputSearchUser = '';
+    } else this.userAlreadyAdded = true;
   }
 
 
-  addNewMember() {
-    this.selectedUsers.forEach(user => {
-      this.channelService.addUserToChannel(this.chatService.currentChatData.channelName, user.uid);
-      this.sendAddMemberMessage(user.user_name);
-    });
-    this.selectedUsers = [];
-    this.choosedUser = null;
+  async addNewMember() {
     this.inputSearchUser = '';
     this.dialogRef.close();
+    for (let user of this.selectedUsers) {
+      await this.channelService.addUserToChannel(this.chatService.currentChatData.channelName, user.uid);
+      await this.sendAddMemberMessage(user.user_name);
+      this.messageService.scrollToBottom();
+  } 
+    this.selectedUsers = [];
+    this.choosedUser = null;
   }
 
 
-  clearInputName(userToRemove) {
-    this.inputSearchUser = ''
-    this.choosedUser = false;
+  removeSelectedUser(userToRemove) {
     this.selectedUsers = this.selectedUsers.filter(user => user.uid !== userToRemove.uid);
   }
 
 
-  async filterUserAllreadyAssigned(): Promise<any> {
+  async filterAlreadyAssignedUsers(): Promise<any> {
     let users = await this.authService.getAllUsers();
     this.channelService.currentChannelData.assignedUsers.forEach((assignedUser: any) => {
       let user = users.find(element => element.uid === assignedUser)
@@ -84,9 +184,9 @@ export class DialogAddMembersComponent implements OnInit {
   }
 
 
-  sendAddMemberMessage(user: string) {
-    this.uploadService.checkForUpload()
+ async sendAddMemberMessage(user: string) {
+   await this.uploadService.checkForUpload()
     this.messageService.messageText = user + ' ist #' + this.channelService.currentChannelData.channelName + ' beigetreten.'
-    this.messageService.newMessage()
+   await this.messageService.newMessage()
   }
 }
